3feaf700dc31b2fb8eedd32fe4ae32f7bf322a7c
Bárbara Oliveira <barbaragdeoliveira@hotmail.com>
2022-07-08 14:48:07 -0300

    desafio finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
-import Footer from "./components/Footer/Footer";
-
-import "./App.css";
+import Skills from "./components/Skills/Skills";
 import Repositories from "./components/Repositories/Repositories";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,9 @@
-import jtmk from "../../img/jtmk.png";
-import maravilhosa from "../../img/maravilhosa.png";
-import seriesPopulares from "../../img/seriesPopulares.png";
-import filmesGhibli from "../../img/filmesGhibli.png";
-import digimons from "../../img/digimons.png";
-import diarioReprograma from "../../img/diarioReprograma.png";
+import jtmk from "../../img/jtmk.gif";
+import maravilhosa from "../../img/maravilhosa.gif";
+import seriesPopulares from "../../img/seriesPopulares.gif";
+import filmesGhibli from "../../img/filmesGhibli.gif";
+import digimons from "../../img/digimons.gif";
+import diarioReprograma from "../../img/diarioReprograma.gif";
 import Card from "../Card/Card";
 import "./Skills.css";
 
@@ -13,38 +13,42 @@ function Skills() {
       <h2 className="skills-title">
         Alguns projetos feitos durante a reprograma
       </h2>
-      <div className="card-list">
-        <Card title="JT Marketing Digital" image={jtmk}>
-          Foi o meu primeiro projeto feito na reprograma, onde aprendi HTNL, CSS
-          e responsividade.
-        </Card>
-        <Card title="M de Maravilhosa" image={maravilhosa}>
-          Desafio elaborado pela reprograma, em que poderiamos escolher a nossa
-          Maravilhosa, sendo ela, uma icone brasileira. Onde pude treinar mais o
-          HTML,CSS,responsividade e o mobile first.
-        </Card>
-        <Card title="Series mais populares" image={seriesPopulares}>
-          Foi minha primeira utilização de uma API e consumindo pelo fetch,
-          mostrando em cards como catálogo de séries e filmes. Utilizei o
-          Boostrap para fazer um carrossel que deu mais dinamismo na página, me
-          desfiando com mais uma ferramenta que não havia usado anteriormente.
-        </Card>
-        <Card title="Filmes Ghibli" image={filmesGhibli}>
-          Utilizei uma api pública e solicitei que retonasse um catálogo de
-          todos os filmes e alguns dados deles como uma galeria de filmes para
-          treinar meus conhecimentos de fetch e css.
-        </Card>
-        <Card title="Digimons" image={digimons}>
-          Foi utilizado uma chamada através do axios de uma API publica e
-          solicitei que o usuário fizesse a busca de todos os digimonse por nome
-          ou por nivel, gerando novos resultados aos continuar pesquisando.
-        </Card>
-        <Card title="Meu diário da reprograma" image={diarioReprograma}>
-          Foi meu primeiro projeto em react. Nele utilizei um pouco de
-          componentes, props e children. Contei um pouco sobre como foi os
-          aprendizados que obtive fazendo o reprograma e o que seria cada uma
-          das ferramentas que me foram disponibilizadas.
-        </Card>
+      <div className="main">
+        <div className="card-list">
+          <Card title="JT Marketing Digital" image={jtmk}>
+            Foi o meu primeiro projeto feito na reprograma, onde aprendi HTML,
+            CSS e responsividade.
+          </Card>
+          <Card title="M de Maravilhosa" image={maravilhosa}>
+            Desafio elaborado pela reprograma, em que poderiamos escolher a
+            nossa Maravilhosa, sendo ela, uma icone brasileira. Onde pude
+            treinar mais o HTML,CSS,responsividade e o mobile first.
+          </Card>
+          <Card title="Series mais populares" image={seriesPopulares}>
+            Foi minha primeira utilização de uma API e consumindo pelo fetch,
+            mostrando em cards como catálogo de séries e filmes. Utilizei o
+            Boostrap para fazer um carrossel que deu mais dinamismo na página,
+            me desfiando com mais uma ferramenta que não havia usado
+            anteriormente.
+          </Card>
+          <Card title="Filmes Ghibli" image={filmesGhibli}>
+            Utilizei uma api pública e solicitei que retonasse um catálogo de
+            todos os filmes e alguns dados deles como uma galeria de filmes para
+            treinar meus conhecimentos de fetch e css.
+          </Card>
+          <Card title="Digimons" image={digimons}>
+            Foi utilizado uma chamada através do axios de uma API publica e
+            solicitei que o usuário fizesse a busca de todos os digimonse por
+            nome ou por nivel, gerando novos resultados aos continuar
+            pesquisando.
+          </Card>
+          <Card title="Meu diário da reprograma" image={diarioReprograma}>
+            Foi meu primeiro projeto em react. Nele utilizei um pouco de
+            componentes, props e children. Contei um pouco sobre como foi os
+            aprendizados que obtive fazendo o reprograma e o que seria cada uma
+            das ferramentas que me foram disponibilizadas.
+          </Card>
+        </div>
       </div>
     </>
   );
